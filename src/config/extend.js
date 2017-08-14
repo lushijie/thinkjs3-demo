78d960d244b335b4466bcb8bc71faeeba892088c
lushijie <lushijie1218@126.com>
2017-08-14 17:20:23 +0800

    cache & session

--- src/config/extend.js
+++ src/config/extend.js
@@ -3,12 +3,16 @@ const model = require('think-model');
 const email = require('think-email');
 const fetch = require('think-fetch');
 const websocket = require('think-websocket');
+const cache = require('think-cache');
+const session = require('think-session');
 
 module.exports = [
   view, //make application support view
   model(think.app),
   email,
   fetch,
-  websocket(think.app)
+  websocket(think.app),
+  cache,
+  session
 ];
 
