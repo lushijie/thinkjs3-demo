17857db310ee1acd4f5c1476e9984ab44e5e81b5
lushijie <lushijie1218@126.com>
2017-08-23 19:05:13 +0800

    mysql

--- src/config/adapter/model.js
+++ src/config/adapter/model.js
@@ -1,15 +1,35 @@
+const isDev = think.app.env === 'development';
+const mysql = require('think-model-mysql');
+//const sequelize = require('think-model')
+
+const DB = {
+  database: 'db_swf',
+  prefix: 'think_',
+  encoding: 'utf8',
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'root',
+  password: 'root'
+}
+
 module.exports = {
   type: 'mysql',
   common: {
-
+    logSql: isDev,
+    logger: msg => console.log(msg)
   },
   mysql: {
-    database: 'db_swf',
-    prefix: 'think_',
-    encoding: 'utf8',
-    host: '127.0.0.1',
-    port: '',
-    user: 'root',
-    password: 'root'
-  }
+    handle: mysql,
+    ...DB
+  },
+  // sequelize: {
+  //   database: 'cdn_agent',
+  //   user: '',
+  //   password: '',
+  //   options: {
+  //     host: '127.0.0.1',
+  //     dialect: 'mysql'
+  //   }
+  // }
 };
+
