afff2f4a03ea7a56cbe119c611943756e29ace4e
lushijie <lushijie1218@126.com>
2017-09-05 19:30:16 +0800

    rest

--- src/controller/rest/rest1.js
+++ src/controller/rest/rest1.js
@@ -1,4 +1,19 @@
 const BaseRest = require('./rest.js');
 module.exports = class extends BaseRest {
+  getAction() {
+    console.log(this.get('id'))
+    console.log('getAction');
+  }
 
+  postAction() {
+    console.log('postAction');
+  }
+
+  putAction() {
+    console.log('putAction');
+  }
+
+  deleteAction(){
+    console.log('deleteAction');
+  }
 };
