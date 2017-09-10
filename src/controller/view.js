1f7ad9fff7226a59e54e27842ab64071addce1b8
lushijie <lushijie1218@126.com>
2017-09-10 14:12:00 +0800

    adapter

--- src/controller/view.js
+++ src/controller/view.js
@@ -2,8 +2,10 @@
 * @Author: lushijie
 * @Date:   2017-08-20 19:32:17
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-09-10 11:49:04
+* @Last Modified time: 2017-09-10 14:11:46
 */
+const ejs = require('think-view-ejs');
+const nunjucks = require('think-view-nunjucks');
 module.exports = class extends think.Controller {
   constructor(...arg) {
     super(...arg);
@@ -22,5 +24,29 @@ module.exports = class extends think.Controller {
 
   index2Action() {
     this.display('view_pug', 'pug')
+  }
+
+  async testAction() {
+    this.assign('name', 'lushijie');
+    let result1 = await this.render('view_ejs', 'ejs');
+    let result2 = await this.render('view_ejs', {type: 'ejs'});
+    let result3 = await this.render('view_nunjucks2', {
+      options: {
+        tags: {
+          blockStart: '<%',
+          blockEnd: '%>',
+          variableStart: '{%',
+          variableEnd: '%}',
+          commentStart: '<#',
+          commentEnd: '#>'
+        }
+      }
+    });
+    let result4 = await this.render('view_ejs', {
+      type: 'ejs',
+      handle: ejs,
+      options: {}
+    });
+    return this.success(result1 + result2 + result3 + result4)
   }
 }
