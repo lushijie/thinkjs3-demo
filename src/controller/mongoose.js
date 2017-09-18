d37608d61a2e0b6eb87e58f2c075ccb553c364d8
lushijie <lushijie1218@126.com>
2017-09-18 09:42:30 +0800

    mongoose

--- src/controller/mongoose.js
+++ src/controller/mongoose.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-08-31 14:56:26
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-09-14 09:33:23
+* @Last Modified time: 2017-09-18 09:42:18
 */
 module.exports = class extends think.Controller {
   constructor(...props) {
@@ -11,9 +11,8 @@ module.exports = class extends think.Controller {
 
   async indexAction() {
     let Player = this.mongoose('mongoose/player', {type: 'mongoose'});
-    let p = new Player({ name: 'lushijie', id: new Date().getTime().toString().slice(4) });
-    p.save();
-    const data = await Player.find();
-    this.json(data);
+    await Player.create({ name: 'lushijie', id: new Date().getTime()});
+    const result = await Player.find();
+    this.json(result);
   }
 }
