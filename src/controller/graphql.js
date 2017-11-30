5d2714907d03789e8e844d97309fabca39164a1f
lushijie <lushijie1218@126.com>
2017-11-30 18:41:17 +0800

    graphql

--- src/controller/graphql.js
+++ src/controller/graphql.js
@@ -2,10 +2,30 @@
 * @Author: lushijie
 * @Date:   2017-11-29 09:27:47
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-11-30 14:02:20
+* @Last Modified time: 2017-11-30 16:29:07
 */
+import {
+  graphql,
+  GraphQLSchema,
 
-const graphql = require('graphql');
+  GraphQLNonNull,
+
+  GraphQLEnumType,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLBoolean,
+  GraphQLInputType,
+  ID,
+
+  buildSchema,
+  printSchema,
+
+  isValidJSValue
+
+} from 'graphql';
+// const graphql = require('graphql');
 
 module.exports = class extends think.Controller {
   constructor(...props) {
@@ -13,21 +33,52 @@ module.exports = class extends think.Controller {
   }
 
   async indexAction() {
-    let queryType = new graphql.GraphQLObjectType({
-      name: 'Query123',
+    return this.display();
+  }
+
+  async searchAction() {
+    let objectQuery = new GraphQLObjectType({
+      name: 'QueryDemo',
       fields: {
         hello: {
-          type: graphql.GraphQLString,
+          type: new GraphQLNonNull(GraphQLString), // GraphQLString GraphQLInt, GraphQLFloat, GraphQLBoolean, ID
           resolve: function () {
-            return 'hello world';
+            return 'hello 666';
+          }
+        },
+        world: {
+          type: GraphQLString,
+          resolve: function () {
+            return 'world 666';
           }
         }
       }
     });
+    let schema = new GraphQLSchema({query: objectQuery});
+    let query = '{ hello }';
+    let result = await graphql(schema, query);
+    console.log(result);
+    //console.log(printSchema(schema));
 
-    let schema = new graphql.GraphQLSchema({query: queryType});
+    let schema2 = buildSchema(`
+      schema {
+        query: QueryDemo
+      }
+      type QueryDemo {
+        hello: String!
+        world: String
+      }
+    `);
 
-    return this.display();
-    // this.json(await graphql.graphql(schema, '{ hello }'));
+    let rootValue = {
+      hello: 'hello123',
+      world: 'world123'
+    }
+
+    let result2 = await graphql(schema2, query, rootValue);
+    console.log(result2);
+
+    console.log(isValidJSValue('aa11223', GraphQLInt))
+    return this.json(result);
   }
 }
